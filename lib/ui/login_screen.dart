449664def39646c9898b71eb420e9634dc457e63
zeeshan90-maker <zeeshankhan557h@gmail.com>
2024-10-03 04:02:31 -0700

    kha

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -1,25 +1,62 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() => LoginScreenState();
-
-}
-
-class LoginScreenState extends State {
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        appBar: AppBar(
-          title: const Text(
-            "Login screen"
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(255, 255, 255, 255),
+      drawer: Drawer(),
+
+      floatingActionButton: Container(
+        height: 60,
+        width: 60,
+        decoration:
+            const BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+        child: const Icon(Icons.message, color: Colors.white),
+      ),
+      appBar: AppBar(
+        automaticallyImplyLeading: true,leading: const Icon(Icons.arrow_back),
+        backgroundColor: Colors.blue, centerTitle: true,
+        title: const Text(
+          "Login Screen",
+          style: TextStyle( color: Colors.white,
+            fontSize: 20,fontWeight: FontWeight.w900,
           ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+          ],
         ),
       ),
     );
   }
+}
 
-
+_container({title}) {
+  return Container(
+    height: 100,
+    width: double.infinity,
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      color: Colors.blue,
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [Text("$title"), Text("Price: \$100")],
+    ),
+  );
 }
